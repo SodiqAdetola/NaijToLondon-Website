6ebf3a3ae0d9336d224fa9ec88ccf6974c59fa4f
sodiq <sa992@student.le.ac.uk>
2024-05-12 17:39:02 +0100

    nav active fix

--- script.js
+++ script.js
@@ -35,9 +35,13 @@ window.onscroll = () => {
     if(top >= offset && top < offset + height) {
         navlinks.forEach(links => {
             links.classList.remove('active');
-            document.querySelector('header nav a[href="#' + id + '"]').classList.add("active");
+            document.querySelector('header .sidebar a[href="#' + id + '"]').classList.add("active");
+            document.querySelector('header .mainbar a[href="#' + id + '"]').classList.add("active");
             return;
             });
+
+        
+        
         };
 
     });
