5dce51699e64dc7b2f248b7280d4b7bd4ebe2116
sodiq <sa992@student.le.ac.uk>
2024-05-13 02:51:40 +0100

    email error message

--- index.js
+++ index.js
@@ -9,10 +9,11 @@ function auth(event) {
     var email = document.getElementById("email").value;
 
     if (email.trim() === '' || !email.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)) {
-      alert("Cannot submit invalid email");
+      alert("Cannot submit invalid email, Email must contain '@' and  '.' and follow a correct email format.");
     }
     else {
     event.preventDefault();
+    alert("Email has been submitted successfully!")
     window.location.assign('page2.html');
     }
   }
